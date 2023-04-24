8fc81d04a713dbd406b98d823de0f565bd017695
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-25 01:30:03 +0530

    feat: don't rate limit in dev

--- backend/src/middleware/rateLimiter.ts
+++ backend/src/middleware/rateLimiter.ts
@@ -1,11 +1,19 @@
+import type { Request, Response, NextFunction } from "express";
 import { rateLimit } from "express-rate-limit";
 
-export const limiter = rateLimit({
-  // 15 minutes
-  windowMs: 15 * 60 * 1000,
+export const limiter =
+  process.env.NODE_ENV === "development"
+    ? (req: Request, res: Response, next: NextFunction) => {
+        // Pass through the middleware without rate limiting in dev mode
+        next();
+      }
+    : rateLimit({
+        // 15 minutes
+        windowMs: 15 * 60 * 1000,
 
-  // limit each IP to 100 requests per windowMs
-  max: 100,
+        // limit each IP to 100 requests per windowMs
+        max: 100,
 
-  message: "Too many requests from this IP, please try again after 15 minutes",
-});
+        message:
+          "Too many requests from this IP, please try again after 15 minutes",
+      });
