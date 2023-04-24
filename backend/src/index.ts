cd2c4fc342c9ea0f85501cc1ec59c4484b727f08
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-24 22:16:44 +0530

    feat: setup entry point

---
+++ backend/src/index.ts
@@ -0,0 +1,43 @@
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import "dotenv/config";
+import { json } from "body-parser";
+import cors from "cors";
+import { connectDB } from "./db/connect";
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+// routes
+app.use("/", (req: Request, res: Response) => {
+  res.send("Backend API!");
+});
+
+// handle errors
+app.use(
+  (err: Error | null, req: Request, res: Response, next: NextFunction) => {
+    return res.status(500).json({
+      message: "Something went wrong!",
+    });
+  }
+);
+
+const start = async () => {
+  try {
+    console.log("Connecting to DB...");
+    await connectDB(process.env.MONGODB_URI as string);
+    console.log("Connected to DB!");
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`server is listening on port ${process.env.PORT || 3000}!`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
