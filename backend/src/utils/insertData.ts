3917081f34f965cdaf2e8150e170df01a497bfa2
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-04-24 22:09:18 +0530

    feat: connect to database

--- backend/src/utils/insertData.ts
+++ backend/src/utils/insertData.ts
@@ -1,7 +1,26 @@
 // used to upload sample data to the database
 
 import fs from "fs";
+import "dotenv/config";
+import mongoose from "mongoose";
+import { connectDB } from "../db/connect";
+import { User } from "../models/user";
 
 const data = JSON.parse(fs.readFileSync("sample_data.json", "utf-8"));
 
-console.log(data);
+async function insertData() {
+  try {
+    console.log("Connecting to DB...");
+    await connectDB(process.env.MONGODB_URI as string);
+    console.log("Connected to DB!");
+
+    await User.insertMany(data);
+    console.log("Data successfully inserted");
+  } catch (error) {
+    console.error("Error inserting data:", error);
+  } finally {
+    mongoose.disconnect();
+  }
+}
+
+insertData();
